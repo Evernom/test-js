3f798997c7a7d9b38549e0fa94d6750eb8d7d03b
Arsen Sarkisian <senya.yg@gmail.com>
2023-01-29 23:53:12 +0200

    f

--- js/script.js
+++ js/script.js
@@ -1,4 +1,79 @@
-"use strict";
+// function filterArray(numbers, value) {
+//     numbers = [];
 
-console.log("Я вчу JS");
-alert(1);
+//     for (let i = 0; i < value; i += 1) {
+      
+//     numbers.push(i);
+//   }
+    
+// return numbers;
+// }
+// const result = filterArray([1, 2, 3, 4, 5], 3);
+
+// console.log(result);
+
+const lastWeekTemps = [1, 2, 3];
+const currentWeekTemps = [4, 5, 6];
+const nextWeekTemps = [7, 8, 9];
+
+const allTemps = [...lastWeekTemps, ...currentWeekTemps, ...nextWeekTemps];
+
+console.log(allTemps);
+
+this.books.splice(this.books.indexOf(oldName), 1, newName);
+
+
+
+
+
+
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    { name: "Stone skin", price: 520 },
+  ],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    const { potions } = this;
+    
+    for (const potion of potions) {
+    if (potion.name === newPotion.name) {
+      return `Error! Potion ${newPotion.name} is already in your inventory!`;
+      }
+    }
+    this.potions.push(newPotion);
+  },
+  removePotion(potionName) {
+    const { potions } = this;
+    
+    for (let i = 0; i < potions.length; i += 1) {
+      const { name } = potions[i];
+      
+      if (potionName === name) {
+       potions.splice(i, 1);
+        
+      }
+      
+     `Potion ${name} is not in inventory!`;
+      
+    }
+  },
+  updatePotionName(oldName, newName) {
+    const { potions } = this;
+    
+    for (let i = 0; i < potions.length; i += 1) {
+      const { name } = potions[i];
+
+    if (oldName === name) {
+      return `Potion ${newName} is not in inventory!`;
+    } else if(oldName === name) {
+      name = newName
+    }
+  }
+  },
+  // Change code above this line
+};
